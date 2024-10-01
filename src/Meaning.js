c85f130260d4a38f9bdc614912d74df321b9659d
analiviag <126291217+analiviag@users.noreply.github.com>
2024-10-01 18:47:02 -0300

    fixed the bug to show the definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,14 +4,11 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <p>{props.meaning.definition}</p>
 
-      {props.meaning.definition.map((index, definition) => (
-        <div key={index}>
-          <p>{definition.definition}</p>
-          <br />
-          <em>{definition.example}</em>
-        </div>
-      ))}
+        <em>{props.meaning.example}</em>
+      </div>
     </div>
   );
 }
